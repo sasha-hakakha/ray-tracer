f3379ba90de300b528105fabb9ff21fe0bfc4700
Sasha Hakakha <sashahakakha@gmail.com>
2022-09-14 19:49:35 -0700

    added half of 3x3 matrix multiplication

--- matrix.cc
+++ matrix.cc
@@ -5,29 +5,41 @@
  */
 
 /* TODO:
- vector to struct? array to struct?*/
-struct {
-  int x;
-  int y;
-  int z;
-} vec3;
+ I'm only doing 4x4 matrix multiplication if I am sure that I need to
+*/
+struct vec3{
+    float x;
+    float y;
+    float z;
+};
 
-struct {
-  int x;
-  int y;
-  int z;
-  int w
-} vec4;
+struct vec4{
+    float x;
+    float y;
+    float z;
+    float w
+};
 
-struct {
-  vec3 r1;
-  vec3 r2;
-  vec3 r3;
-} m3;
+struct m3{
+    vec3 r1;
+    vec3 r2;
+    vec3 r3;
+};
 
-struct {
-  vec4 r1;
-  vec4 r2;
-  vec4 r3;
-  vec4 r4;
-} m4;
+struct m4{
+    vec4 r1;
+    vec4 r2;
+    vec4 r3; 
+    vec4 r4;
+};
+
+m3 mm3 (m4 one, m4 two){
+    m3 result;
+    // top left, one top row by two left col
+    result.r1.x = one.r1.x * two.r1.x + one.r1.y * two.r2.x + one.r1.z * two.r3.x;
+    // top mid, one top row by two middle col
+    result.r1.y = one.r1.x * two.r1.y + one.r1.y * two.r2.y + one.r1.z + two.r3.y;
+    // top right, one top row by two right col
+    result.r1.z = one.r1.x * two.r1.z * one.r1.y * two.r2.z + one.r1.z + two.r3.z;
+    // mid left
+}
