f1fab23c52110318f4f204dc4be176c77838c27c
Sasha Hakakha <sashahakakha@gmail.com>
2022-12-24 17:16:20 -0800

    Halfway done reformat to use arrays for verts

--- geometry.rs
+++ geometry.rs
@@ -1,80 +1,130 @@
 #![allow(dead_code)]
-#[derive(Copy, Clone)]
+
+// extern crate alloc;
+// use alloc::{vec::Vec, string::String};
+// extern crate core;
+// use core::option::Option::{self,*};
+// use core::iter::Iterator;
+// use core::panic::Panic;
+
 mod matrix;
-use std:ptr;
 
-pub struct Vert3{
-    coords: matrix::Vert3,
+
+
+// pub fn _many_points_to_tri(verts:&[matrix::Vert3], points: Vec<&str>) -> Vec<matrix::Face3M> {
+//     let mut faces = Vec::new();
+//     for i in 0..points.len() - 2 {
+//         faces.push(matrix::Face3M{
+//             col1:*verts[points[0].parse::<usize>().unwrap()], 
+//             col2:*verts[points[i].parse::<usize>().unwrap()], 
+//             col3:*verts[points[i + 1].parse::<usize>().unwrap()]
+//             }
+//         );
+//     }
+//     faces
+// }
+pub fn many_points_to_tri(verts:&[matrix::Vert3], points: &[&str]) -> Vec<matrix::Face3M> {
+    let mut faces = Vec::new();
+    for i in 0..points.len() - 2 {
+        faces.push(matrix::Face3M{
+            col1:verts[points[0].parse::<usize>().unwrap()], 
+            col2:verts[points[i].parse::<usize>().unwrap()],
+            col3:verts[points[i + 1].parse::<usize>().unwrap()]
+            }
+        );
+    }
+    faces
 }
+// pub fn three_verts_to_face(points: Vec<String>, verts: &[Vert3]) -> Face {
+//     Face3M { 
+//         col1: verts[points[0].parse::<i32>().unwrap()],
+//         col2: verts[points[1].parse::<i32>().unwrap()],
+//         col3: verts[points[2].parse::<i32>().unwrap()]
+//     }
+// }
 
-pub struct TextureCoord{
-    coords: matrix::Vert3,
-}
+// pub fn three_verts_to_face(points: Vec<&str>, verts: &[matrix::Vert3]) -> matrix::Face3M {
+//     matrix::Face3M { 
+//         col1: verts[points[0].parse::<usize>().unwrap()],
+//         col2: verts[points[1].parse::<usize>().unwrap()],
+//         col3: verts[points[2].parse::<usize>().unwrap()]
+//     }
+// }
+pub fn three_verts_to_face(points: Vec<&str>, verts: &Vec<matrix::Vert3>) -> matrix::Face3M {
+    let col1_index = points[0].parse::<usize>().unwrap();
+    let col2_index = points[1].parse::<usize>().unwrap();
+    let col3_index = points[2].parse::<usize>().unwrap();
 
-pub struct VertNorm{
-    coords: matrix::Vert3,
-}
-
-pub struct SpaceVert{
-    coords: matrix::Vert3,
-}
-
-pub struct Face3{
-    verts: matrix::M3,
-}
-
-pub struct PlusThreeFace{
-    verts: Vec<Vert3>,
-}
-
-pub fn process_vert3(s: String) -> Vert{
-    let verts = s.split(" ");
-    Vert3{
-        x: verts[1].parse::<f32>,
-        y: verts[2].parse::<f32>,
-        z: verts[3].parse::<f32>,
+    matrix::Face3M {
+        col1: verts[col1_index],
+        col2: verts[col2_index],
+        col3: verts[col3_index]
     }
 }
 
 pub fn process_line(
     s: String,
-    verts: Vec<Vert>,
-    texture_coords: Vec<TextureCoord>,
-    vert_norms: Vec<VertNorm>,
-    faces: Vec<Face3>){
+    verts: &Vec<matrix::Vert3>,
+    texture_coords: Vec<matrix::TextureCoord>,
+    vert_norms: Vec<matrix::VertNorm>,
+    faces: Vec<matrix::Face3M>){
 
-    let parts = s.split(" ");
-    let delimeter = parts[0].parse::<i32>;
+    let parts = s.split(" ").collect::<Vec<&str>>();
+    let delimeter = parts[0];
+
     if delimeter == "v"{
-        verts.push(Vert3{
-            coords.x: parts[1].parse::<f32>,
-            coords.y: parts[2].parse::<f32>,
-            coords.z: parts[3].parse::<f32>,
+        verts.push(matrix::Vert3{
+            coords:vec!{
+                parts[1].parse::<f32>().unwrap(),
+                parts[2].parse::<f32>().unwrap(),
+                parts[3].parse::<f32>().unwrap(),
+            }
         });
     }else if delimeter == "vt"{
-        if parts.length() == 1{
-            texture_coords.push(TextureCoord{
-                coords.x: parts[1].parse::<f32>,
-                ..Default::default() }
-            );
-        } else if parts.lengt() == 2{
-            texture_coords.push(TextureCoord{
-                coords.x: parts[1].parse::<f32>,
-                coords.y: parts[2].parse::<f32>,
-                ..Default::default() }
-            );
+        if parts.len() == 1{
+            texture_coords.push(matrix::TextureCoord{
+                coords:vec!{
+                    parts[1].parse::<f32>().unwrap(),
+                    -1.0, -1.0
+                }
+            });
+        } else if parts.len() == 2{
+            texture_coords.push(matrix::TextureCoord{
+                coords:vec!{
+                    parts[1].parse::<f32>().unwrap(),
+                    parts[2].parse::<f32>().unwrap(),
+                    -1.0,
+                }
+            });
         } else{
-            texture_coords.push(TextureCoord{
-                coords.x: parts[1].parse::<f32>,
-                coords.y: parts[2].parse::<f32>,
-                coords.z: parts[3].parse::<f32>,}
+            texture_coords.push(matrix::TextureCoord{
+                coords:vec!{
+                    parts[1].parse::<f32>().unwrap(),
+                    parts[2].parse::<f32>().unwrap(),
+                    parts[3].parse::<f32>().unwrap(),
+                }
+            });
+        }
+    }
+    else if delimeter == "f"{
+        if parts.len() < 3 {panic!("Face has less than 3 points!")}
+        else if parts.len() == 3{
+            faces.push(three_verts_to_face(
+                parts,
+                verts,
+            ));
+        }
+        else{
+            let mut new =  many_points_to_tri(
+                verts,
+                &parts,
             );
-        
+            faces.append(&mut new);
+        }
+    }
+
+} 
             
-                             
-
-}       
-
 fn main(){
     println!("geometry good!");
 }
--- matrix.rs
+++ matrix.rs
@@ -1,169 +1,254 @@
+//#![no_implicit_prelude]
 #![allow(dead_code)]
-#[derive(Copy, Clone)]
-
-use ptr;
-
-pub struct Vec3{
-    x: Option<f32>,
-    y: Option<f32>,
-    z: Option<f32>,
+// extern crate alloc;
+// use alloc::vec::Vec;
+#[derive(Debug, Clone)]
+pub struct Vert {
+    coords: [f32; 4],
+}
+pub struct Plane {
+    coord: Vert,
+    norm: Vert,
 }
 
-impl Default for Vec3{
-    fn default() -> Vec3 {
-        Vec3{
-            x: None,
-            y: None,
-            z: None,
-        }
-    }
+pub struct M3 {
+    col1: Vert,
+    col2: Vert,
+    col3: Vert,
 }
 
-pub struct Vec4{
-    x: Option<f32>,
-    y: Option<f32>,
-    z: Option<f32>,
-    w: Option<f32>,
+pub struct M4 {
+    col1: Vert,
+    col2: Vert,
+    col3: Vert,
+    col4: Vert,
+}
+pub struct Ray {
+    point: Vert,
+    vert: Vert,
 }
 
-pub struct V3Ray{
-    point: Vec3,
-    vert: Vec3,
+pub struct TextureCoord {
+    coords: Vert,
 }
 
-
-pub struct Plane{
-    coord: Vec3,
-    norm: Vec3,
+pub struct VertNorm {
+    coords: Vert,
 }
 
-pub struct M3{
-    col1: Vec3,
-    col2: Vec3,
-    col3: Vec3,
+pub struct SpaceVert {
+    coords: Vert,
 }
 
-pub struct M4{
-    col1: Vec4,
-    col2: Vec4,
-    col3: Vec4,
-    col4: Vec4,
+// pub struct Face3{
+//     verts: matrix::M3,
+// }
+
+pub struct Face3 {
+    verts: M3,
 }
 
-pub fn dot_v3v3(one: Vec3, two: Vec3) -> f32{
-    one.x * two.x + one.y * two.y + one.z + two.z
+pub fn dot_v3v3(one: &[f32; 4], two: &[f32; 4]) -> f32 {
+    one[0] * two[0] + one[1] * two[1] + one[2] + two[2]
 }
 
-pub fn cross_v3v3(one: Vec3, two: Vec3) -> Vec3 {
-    Vec3{
-        x: one.y * two.z - one.z * two.y,
-        y: one.x * two.z - one.z * two.x,
-        z: one.x * two.y - one.y * two.x,
-    }
+pub fn cross_v3v3(one: &[f32; 4], two: &[f32; 4]) -> [f32; 4] {
+    vec![
+        one[1] * two[2] - one[2] * two[1],
+        one[0] * two[2] - one[2] * two[0],
+        one[0] * two[1] - one[1] * two[0],
+    ]
 }
 
-pub fn add_v3v3(one: Vec3, two: Vec3) -> Vec3 {
-    Vec3{
-        x: one.x + two.x,
-        y: one.y + two.y,
-        z: one.z + two.z,
-    }
+pub fn add_v3v3(one: &[f32; 4], two: &[f32; 4]) -> [f32; 4] {
+    vec![one[0] + two[0], one[1] + two[1], one[2] + two[2]]
 }
 
-pub fn sub_v3v3(one: Vec3, two: Vec3) -> Vec3 {
-    Vec3{
-        x: one.x - two.x,
-        y: one.y - two.y,
-        z: one.z - two.z,
-    }
-}  
-
-pub fn mul_v3fl(v: Vec3, f: f32) -> Vec3{
-    Vec3{
-        x: v.x * f,
-        y: v.y * f,
-        z: v.z * f,
-    }
+pub fn sub_v3v3(one: &[f32; 4], two: &[f32; 4]) -> [f32; 4] {
+    vec![one[0] - two[0], one[1] - two[1], one[2] - two[2]]
 }
 
-pub fn points_to_plane(one: Vec3, two: Vec3, three: Vec3) -> Plane{
-    let coord = cross_v3v3(sub_v3v3(two, one), sub_v3v3(three, one));
-    Plane{
+pub fn mul_v3fl(v: &[f32; 4], f: &f32) -> [f32; 4] {
+    vec![v[0] * f, v[1] * f, v[2] * f]
+}
+
+pub fn points_to_plane(one: Vert, two: Vert, three: Vert) -> Plane {
+    let coord = cross_v3v3(&sub_v3v3(&two, &one), &sub_v3v3(&three, &one));
+    Plane {
         coord: coord,
         norm: one,
     }
 }
 
-pub fn ray_isect_plane(r: V3Ray, pl: Plane) -> Vec3{
-    let dot = dot_v3v3(pl.norm, r.vert);
-    let w = sub_v3v3(r.point, pl.coord);
-    let dot2 = dot_v3v3(pl.norm, w);
+pub fn ray_isect_plane(r: Ray, pl: Plane) -> Vec<f32> {
+    let dot = dot_v3v3(&pl.norm, &r.vert);
+    let w = sub_v3v3(&r.point, &pl.coord);
+    let dot2 = dot_v3v3(&pl.norm, &w);
     let fac = -1.0 * dot2 / dot;
-    let u = mul_v3fl(r.vert, fac);
-    add_v3v3(r.point, u)
+    let u = mul_v3fl(&r.vert, &fac);
+    add_v3v3(&r.point, &u)
 }
 
-pub fn dist_v3v3(a: Vec3, b: Vec3) -> f32{
-   ((a.x-b.x).powf(2.0) + (a.y-b.y).powf(2.0) + (a.z-b.z).powf(2.0)).sqrt()
+pub fn dist_v3v3(a: Vert, b: Vert) -> f32 {
+    ((a[0] - b[0]).powf(2.0) + (a[1] - b[1]).powf(2.0) + (a[2] - b[2]).powf(2.0)).sqrt()
 }
 
-pub fn p_inside_tri(p: Vec3, v1: Vec3, v2: Vec3, v3: Vec3) -> bool{
-    let d1 = dist_v3v3(p, v1);
-    let d2 = dist_v3v3(p, v2);
-    let d3 = dist_v3v3(p, v3);
+pub fn p_inside_tri(p: Vert, v1: Vert, v2: Vert, v3: Vert) -> bool {
+    let d1 = dist_v3v3(&p, &v1);
+    let d2 = dist_v3v3(&p, &v2);
+    let d3 = dist_v3v3(&p, &v3);
     let total = d1 + d2 + d3;
-    d1 > 0 && d2 > 0 && d3 > 0 && d1 < total && d2 < total && d3 < total
+    d1 > 0.0 && d2 > 0.0 && d3 > 0.0 && d1 < total && d2 < total && d3 < total
 }
 
-pub fn m3m(one: M3, two: M3){
-    let col1 = Vec3{
-        x: one.col1.x * two.col1.x + one.col2.x * two.col1.y + one.col3.x * two.col1.z,
-        y: one.col1.y * two.col1.x + one.col2.y * two.col1.y + one.col3.y * two.col1.z,
-        z: one.col1.z * two.col1.x + one.col2.z * two.col1.y + one.col3.z * two.col1.z,
+pub fn m3m(one: M3, two: M3) -> M3 {
+    let col1 = Vert {
+        coords: [
+            one.col1.coords[0] * two.col1.coords[0]
+                + one.col2.coords[0] * two.col1.coords[1]
+                + one.col3.coords[0] * two.col1.coords[2],
+            one.col1.coords[1] * two.col1.coords[0]
+                + one.col2.coords[1] * two.col1.coords[1]
+                + one.col3.coords[1] * two.col1.coords[2],
+            one.col1.coords[2] * two.col1.coords[0]
+                + one.col2.coords[2] * two.col1.coords[1]
+                + one.col3.coords[2] * two.col1.coords[2],
+            -1.0,
+        ],
     };
-    let col2 = Vec3{
-        x: one.col1.x * two.col2.x + one.col2.x * two.col2.y + one.col3.x * two.col2.z,
-        y: one.col1.y * two.col2.x + one.col2.y * two.col2.y + one.col3.y * two.col2.z,
-        z: one.col1.z * two.col2.x + one.col2.z * two.col2.y + one.col3.z * two.col2.z,
+    let col2 = Vert {
+        coords: [
+            one.col1.coords[0] * two.col2.coords[0]
+                + one.col2.coords[0] * two.col2.coords[1]
+                + one.col3.coords[0] * two.col2.coords[2],
+            one.col1.coords[1] * two.col2.coords[0]
+                + one.col2.coords[1] * two.col2.coords[1]
+                + one.col3.coords[1] * two.col2.coords[2],
+            one.col1.coords[2] * two.col2.coords[0]
+                + one.col2.coords[2] * two.col2.coords[1]
+                + one.col3.coords[2] * two.col2.coords[2],
+            -1.0,
+        ],
     };
-    let col3 = Vec3{
-        x: one.col1.x * two.col3.x + one.col2.x * two.col3.y + one.col3.x * two.col3.z,
-        y: one.col1.y * two.col3.x + one.col2.y * two.col3.y + one.col3.y * two.col3.z,
-        z: one.col1.z * two.col3.x + one.col2.z * two.col3.y + one.col3.z * two.col3.z,
+    let col3 = Vert {
+        coords: [
+            one.col1.coords[0] * two.col3.coords[0]
+                + one.col2.coords[0] * two.col3.coords[1]
+                + one.col3.coords[0] * two.col3.coords[2],
+            one.col1.coords[1] * two.col3.coords[0]
+                + one.col2.coords[1] * two.col3.coords[1]
+                + one.col3.coords[1] * two.col3.coords[2],
+            one.col1.coords[2] * two.col3.coords[0]
+                + one.col2.coords[2] * two.col3.coords[1]
+                + one.col3.coords[2] * two.col3.coords[2],
+            -1.0,
+        ],
     };
-    M3{
+    M3 {
         col1: col1,
         col2: col2,
         col3: col3,
-    }   
-}   
+    }
+}
 
-pub fn m4m(one: M4, two: M4){
-    let col1 = Vec4{
-        x: one.col1.x + two.col1.x + one.col2.x * two.col1.y + one.col3.x * two.col1.z + one.col4.x * two.col1.w,
-        y: one.col1.y + two.col1.x + one.col2.y * two.col1.y + one.col3.y * two.col1.z + one.col4.y * two.col1.w,
-        z: one.col1.z + two.col1.x + one.col2.z * two.col1.y + one.col3.z * two.col1.z + one.col4.z * two.col1.w,
-        w: one.col1.w + two.col1.x + one.col2.w * two.col1.y + one.col3.w * two.col1.z + one.col4.w * two.col1.w,
+pub fn m4m(one: M4, two: M4) -> M4 {
+    let col1 = Vert {
+        coords: [
+            one.col1.coords[0]
+                + two.col1.coords[0]
+                + one.col2.coords[0] * two.col1.coords[1]
+                + one.col3.coords[0] * two.col1.coords[2]
+                + one.col4.coords[0] * two.col1.coords[3],
+            one.col1.coords[1]
+                + two.col1.coords[0]
+                + one.col2.coords[1] * two.col1.coords[1]
+                + one.col3.coords[1] * two.col1.coords[2]
+                + one.col4.coords[1] * two.col1.coords[3],
+            one.col1.coords[2]
+                + two.col1.coords[0]
+                + one.col2.coords[2] * two.col1.coords[1]
+                + one.col3.coords[2] * two.col1.coords[2]
+                + one.col4.coords[2] * two.col1.coords[3],
+            one.col1.coords[3]
+                + two.col1.coords[0]
+                + one.col2.coords[3] * two.col1.coords[1]
+                + one.col3.coords[3] * two.col1.coords[2]
+                + one.col4.coords[3] * two.col1.coords[3],
+        ],
     };
-    let col2 = Vec4{
-        x: one.col1.x + two.col2.x + one.col2.x * two.col2.y + one.col3.x * two.col2.z + one.col4.x * two.col2.w,
-        y: one.col1.y + two.col2.x + one.col2.y * two.col2.y + one.col3.y * two.col2.z + one.col4.y * two.col2.w,
-        z: one.col1.z + two.col2.x + one.col2.z * two.col2.y + one.col3.z * two.col2.z + one.col4.z * two.col2.w,
-        w: one.col1.w + two.col2.x + one.col2.w * two.col2.y + one.col3.w * two.col2.z + one.col4.w * two.col2.w,
+    let col2 = Vert {
+        coords: [
+            one.col1.coords[0]
+                + two.col2.coords[0]
+                + one.col2.coords[0] * two.col2.coords[1]
+                + one.col3.coords[0] * two.col2.coords[2]
+                + one.col4.coords[0] * two.col2.coords[3],
+            one.col1.coords[1]
+                + two.col2.coords[0]
+                + one.col2.coords[1] * two.col2.coords[1]
+                + one.col3.coords[1] * two.col2.coords[2]
+                + one.col4.coords[1] * two.col2.coords[3],
+            one.col1.coords[2]
+                + two.col2.coords[0]
+                + one.col2.coords[2] * two.col2.coords[1]
+                + one.col3.coords[2] * two.col2.coords[2]
+                + one.col4.coords[2] * two.col2.coords[3],
+            one.col1.coords[3]
+                + two.col2.coords[0]
+                + one.col2.coords[3] * two.col2.coords[1]
+                + one.col3.coords[3] * two.col2.coords[2]
+                + one.col4.coords[3] * two.col2.coords[3],
+        ],
     };
-    let col3 = Vec4{
-        x: one.col1.x + two.col3.x + one.col2.x * two.col3.y + one.col3.x * two.col3.z + one.col4.x * two.col3.w,
-        y: one.col1.y + two.col3.x + one.col2.y * two.col3.y + one.col3.y * two.col3.z + one.col4.y * two.col3.w,
-        z: one.col1.z + two.col3.x + one.col2.z * two.col3.y + one.col3.z * two.col3.z + one.col4.z * two.col3.w,
-        w: one.col1.w + two.col3.x + one.col2.w * two.col3.y + one.col3.w * two.col3.z + one.col4.w * two.col3.w,
+    let col3 = Vert {
+        coords: [
+            one.col1.coords[0]
+                + two.col3.coords[0]
+                + one.col2.coords[0] * two.col3.coords[1]
+                + one.col3.coords[0] * two.col3.coords[2]
+                + one.col4.coords[0] * two.col3.coords[3],
+            one.col1.coords[1]
+                + two.col3.coords[0]
+                + one.col2.coords[1] * two.col3.coords[1]
+                + one.col3.coords[1] * two.col3.coords[2]
+                + one.col4.coords[1] * two.col3.coords[3],
+            one.col1.coords[2]
+                + two.col3.coords[0]
+                + one.col2.coords[2] * two.col3.coords[1]
+                + one.col3.coords[2] * two.col3.coords[2]
+                + one.col4.coords[2] * two.col3.coords[3],
+            one.col1.coords[3]
+                + two.col3.coords[0]
+                + one.col2.coords[3] * two.col3.coords[1]
+                + one.col3.coords[3] * two.col3.coords[2]
+                + one.col4.coords[3] * two.col3.coords[3],
+        ],
     };
-    let col2 = Vec4{
-        x: one.col1.x + two.col4.x + one.col2.x * two.co4.y + one.col3.x * two.co4.z + one.col4.x * two.co4.w,
-        y: one.col1.y + two.col4.x + one.col2.y * two.co4.y + one.col3.y * two.co4.z + one.col4.y * two.co4.w,
-        z: one.col1.z + two.col4.x + one.col2.z * two.co4.y + one.col3.z * two.co4.z + one.col4.z * two.co4.w,
-        w: one.col1.w + two.col4.x + one.col2.w * two.co4.y + one.col3.w * two.co4.z + one.col4.w * two.co4.w,
+    let col4 = Vert {
+        coords: [
+            one.col1.coords[0]
+                + two.col4.coords[0]
+                + one.col2.coords[0] * two.col4.coords[1]
+                + one.col3.coords[0] * two.col4.coords[2]
+                + one.col4.coords[0] * two.col4.coords[3],
+            one.col1.coords[1]
+                + two.col4.coords[0]
+                + one.col2.coords[1] * two.col4.coords[1]
+                + one.col3.coords[1] * two.col4.coords[2]
+                + one.col4.coords[1] * two.col4.coords[3],
+            one.col1.coords[2]
+                + two.col4.coords[0]
+                + one.col2.coords[2] * two.col4.coords[1]
+                + one.col3.coords[2] * two.col4.coords[2]
+                + one.col4.coords[2] * two.col4.coords[3],
+            one.col1.coords[3]
+                + two.col4.coords[0]
+                + one.col2.coords[3] * two.col4.coords[1]
+                + one.col3.coords[3] * two.col4.coords[2]
+                + one.col4.coords[3] * two.col4.coords[3],
+        ],
     };
-    M4{
+    M4 {
         col1: col1,
         col2: col2,
         col3: col3,
@@ -171,6 +256,6 @@ pub fn m4m(one: M4, two: M4){
     }
 }
 
-fn main(){
+fn main() {
     println!("done!");
 }
