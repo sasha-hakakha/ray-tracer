113be8308d1a790bb0b88a5049fb89f30090816e
Sasha Hakakha <sashahakakha@gmail.com>
2022-12-24 19:39:27 -0800

    Refactored matrix to use arr

--- matrix.rs
+++ matrix.rs
@@ -53,50 +53,59 @@ pub fn dot_v3v3(one: &[f32; 4], two: &[f32; 4]) -> f32 {
 }
 
 pub fn cross_v3v3(one: &[f32; 4], two: &[f32; 4]) -> [f32; 4] {
-    vec![
+    [
         one[1] * two[2] - one[2] * two[1],
         one[0] * two[2] - one[2] * two[0],
         one[0] * two[1] - one[1] * two[0],
+        -1.0,
     ]
 }
 
 pub fn add_v3v3(one: &[f32; 4], two: &[f32; 4]) -> [f32; 4] {
-    vec![one[0] + two[0], one[1] + two[1], one[2] + two[2]]
+    [one[0] + two[0], one[1] + two[1], one[2] + two[2], -1.0]
 }
 
 pub fn sub_v3v3(one: &[f32; 4], two: &[f32; 4]) -> [f32; 4] {
-    vec![one[0] - two[0], one[1] - two[1], one[2] - two[2]]
+    [one[0] - two[0], one[1] - two[1], one[2] - two[2], -1.0]
 }
 
 pub fn mul_v3fl(v: &[f32; 4], f: &f32) -> [f32; 4] {
-    vec![v[0] * f, v[1] * f, v[2] * f]
+    [v[0] * f, v[1] * f, v[2] * f, -1.0]
 }
 
-pub fn points_to_plane(one: Vert, two: Vert, three: Vert) -> Plane {
-    let coord = cross_v3v3(&sub_v3v3(&two, &one), &sub_v3v3(&three, &one));
+pub fn points_to_plane(one: &Vert, two: &Vert, three: &Vert) -> Plane {
+    let coord = Vert {
+        coords: cross_v3v3(
+            &sub_v3v3(&two.coords, &one.coords),
+            &sub_v3v3(&three.coords, &one.coords),
+        ),
+    };
+    let norm = one.clone();
     Plane {
         coord: coord,
-        norm: one,
+        norm: norm,
     }
 }
 
-pub fn ray_isect_plane(r: Ray, pl: Plane) -> Vec<f32> {
-    let dot = dot_v3v3(&pl.norm, &r.vert);
-    let w = sub_v3v3(&r.point, &pl.coord);
-    let dot2 = dot_v3v3(&pl.norm, &w);
+pub fn ray_isect_plane(r: &Ray, pl: &Plane) -> Vert {
+    let dot = dot_v3v3(&pl.norm.coords, &r.vert.coords);
+    let w = sub_v3v3(&r.point.coords, &pl.coord.coords);
+    let dot2 = dot_v3v3(&pl.norm.coords, &w);
     let fac = -1.0 * dot2 / dot;
-    let u = mul_v3fl(&r.vert, &fac);
-    add_v3v3(&r.point, &u)
+    let u = mul_v3fl(&r.vert.coords, &fac);
+    Vert {
+        coords: add_v3v3(&r.point.coords, &u),
+    }
 }
 
-pub fn dist_v3v3(a: Vert, b: Vert) -> f32 {
+pub fn dist_v3v3(a: [f32; 4], b: [f32; 4]) -> f32 {
     ((a[0] - b[0]).powf(2.0) + (a[1] - b[1]).powf(2.0) + (a[2] - b[2]).powf(2.0)).sqrt()
 }
 
-pub fn p_inside_tri(p: Vert, v1: Vert, v2: Vert, v3: Vert) -> bool {
-    let d1 = dist_v3v3(&p, &v1);
-    let d2 = dist_v3v3(&p, &v2);
-    let d3 = dist_v3v3(&p, &v3);
+pub fn p_inside_tri(p: &Vert, v1: &Vert, v2: &Vert, v3: &Vert) -> bool {
+    let d1 = dist_v3v3(p.coords, v1.coords);
+    let d2 = dist_v3v3(p.coords, v2.coords);
+    let d3 = dist_v3v3(p.coords, v3.coords);
     let total = d1 + d2 + d3;
     d1 > 0.0 && d2 > 0.0 && d3 > 0.0 && d1 < total && d2 < total && d3 < total
 }
